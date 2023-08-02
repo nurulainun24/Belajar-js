465d178b8830d9a69046da6625a21c7fe4c42beb
nurulainun24 <nurul.ainun@valutac.onmicrosoft.com>
2023-08-02 17:06:58 +0800

    membuat variabel

---
+++ Belajar-js.js
@@ -0,0 +1,28 @@
+var x = 10;
+var y = 200;
+
+console.log ( "x lama:",x );
+console.log ( "y lama",y );
+
+x = 200;
+y = 10;
+
+console.log ( "x baru:",x );
+console.log ( "y baru:",y)
+
+var a = 3;
+var b = 24;
+
+x = a
+y = b
+
+console.log ( "x:",a );
+console.log ( "y:",b );
+
+var nama = "ainun";
+var n = 5;
+var angka1 = 1234.5678;
+
+console.log ( nama );
+console.log ( "n:",n );
+console.log ( "angka1:",angka1 );
