37f3545e233c12291aacb403494a8f71c606635c
nurulainun24 <nurul.ainun@valutac.onmicrosoft.com>
2023-08-03 11:54:35 +0800

    belajar control-flow

---
+++ control-flow.js
@@ -0,0 +1,21 @@
+var nilai = 10;
+
+if ( nilai >= 90 ) {
+    console.log("A")
+} else if ( nilai >=85 ) {
+    console.log ("B")
+} else if ( nilai >= 80 ) {
+    console.log ("C")
+} else if ( nilai >= 70 ) {
+    console.log ("D")
+}else {
+    console.log ("E")
+}
+
+// 1. buat variable nilai: number
+// 2. print grade
+// 90 >= A
+// 85 - 90 B
+// 80 - 85 C
+// 70 - 80 D
+// < 70 E
