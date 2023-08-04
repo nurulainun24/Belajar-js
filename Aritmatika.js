f271ea455c3088e44ef9400d5d96e95249da9217
nurulainun24 <nurul.ainun@valutac.onmicrosoft.com>
2023-08-04 15:50:08 +0800

    belajar aritmatika js

---
+++ Aritmatika.js
@@ -0,0 +1,40 @@
+//operator
+var x = 2;
+var y = 7;
+
+var z = x + y;
+console.log ("nilai z adalah:", z);
+
+var a = 15;
+var b = 5;
+
+var c = a - b;
+console.log ("nilai c adalah:", c);
+
+var e = 10;
+var f = 10;
+
+var g = e + f;
+console.log ("nilai g adalah:", g);
+
+var m = 100;
+var n = 50;
+
+var o = m / n;
+console.log ("nilai o adalah:", o);
+
+//modulus
+//menghitung jumlah sisa
+var x = 2 % 3;
+console.log ("nilai x adalah",x)
+
+//string
+var x = 10;
+var y = "100";
+
+var z = y - x;
+console.log ("nilai z adalah:", z);
+
+//desimal
+var z = 3.6 + 2.3;
+console.log("nilai z adalah", z)
